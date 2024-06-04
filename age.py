30ea67e6af822d4d909f50ea4e0d4fe15b754b4e
alone <154563439+neverfriendme@users.noreply.github.com>
2024-06-04 19:35:55 -0400

    Update age.py

--- age.py
+++ age.py
@@ -1,6 +1,7 @@
 Birth_year = int(input("Birth year :"))
 year = int(input("year :"))
 Month_now = int(input("month now :"))
-Month = Month_now
+Month_birth = int(input("month of birth: "))
+Month = Month_now - 12 + Month_birth
 Result = year - Birth_year
 print(f"you are,{Result} years old and {Month} months old")
