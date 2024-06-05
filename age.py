ccac0252a12a39b580883412feef299c629fdf4b
alone <154563439+neverfriendme@users.noreply.github.com>
2024-06-05 19:17:34 -0400

    Update age.py

--- age.py
+++ age.py
@@ -4,9 +4,9 @@ Month_now = int(input("month now :"))
 Month_birth = int(input("month of birth: "))
 Month = Month_birth - Month_now
 Result = year - Birth_year
-Late = int(input("12,11,10,9,8,0 if you have a late birth-day please enter it again if not please press 0 :\n"));
-if Month_birth == Late:
-        while True:
+Late = int(input("12,11,10,9,8,0 : "));
+Month_birth2 =int(input("Your Month of birth again :"))
+if Month_birth2 == Late:
+        if True:
                 Result - 1
 print(f"you are,{Result} years old and {Month} months old")
-
